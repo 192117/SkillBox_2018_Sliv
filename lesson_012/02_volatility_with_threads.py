9221d74221ea5578abeac3c3d054eb294df6fcb5
kalimullin_workspace <noti1996@gmail.com>
2021-08-12 16:08:57 +0300

    Version 1. Need check.

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -21,4 +21,66 @@
 
 # TODO тут ваш код в многопоточном стиле
 
-# TODO переписать однопоточный, чтобы лучше было для многопоточки
+import threading
+import os
+import csv
+
+def parser_directory(directorys, data):
+    for file_directorys in os.walk(directorys):
+        os.chdir(directorys)
+        for file in file_directorys[2]:
+            data.append(file)
+
+class Volatility(threading.Thread):
+
+    def __init__(self, file, directory, lock1, lock2, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file = file
+        self.directory = directory
+        self.traders = dict()
+        self.traders_lock = lock1
+        self.traders_zero = dict()
+        self.traders_zero_lock = lock2
+
+    def run(self):
+        os.chdir(self.directory)
+        for traders in self.file:
+            with open(traders) as file_traders:
+                file_csv = csv.reader(file_traders, delimiter=",")
+                max_price = 0
+                min_price = 0
+                next(file)
+                for row in file_csv:
+                    secid = row[0]
+                    price = float(row[2])
+                    if price > max_price:
+                        if min_price == 0:
+                            min_price = price
+                        max_price = price
+                    elif price < min_price:
+                        min_price = price
+                medium_price = (max_price + min_price) / 2
+                volatility = ((max_price - min_price) / medium_price) * 100
+                if volatility > 0.0:
+                    self.traders_lock.acquire()
+                    self.traders[secid] = float(format(volatility, ".2f"))
+                    self.traders_lock.release()
+                else:
+                    self.traders_zero_lock.acquire()
+                    self.traders_zero[secid] = float(format(volatility, ".2f"))
+                    self.traders_zero_lock.release()
+
+if __name__ == "__main__":
+    os.chdir("C:\\Users\\kalimullin\\PycharmProjects\\SkillBox_2018_Sliv\\lesson_012\\trades")
+    base_data = list()
+    parser_directory("C:\\Users\\kalimullin\\PycharmProjects\\SkillBox_2018_Sliv\\lesson_012\\trades", base_data)
+    lock1 = threading.Lock()
+    lock2 = threading.Lock()
+    volatility = [Volatility(file=file,
+                      directory="C:\\Users\\kalimullin\\PycharmProjects\\SkillBox_2018_Sliv\\lesson_012\\trades",
+                      lock1=lock1, lock2=lock2) for file in base_data]
+    for vol in volatility:
+        vol.start()
+    for vol in volatility:
+        vol.join()
+
