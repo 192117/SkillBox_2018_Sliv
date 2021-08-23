9dbc92689d71277a0517f26886ab404703628eb5
Kokoc <noti1996@gmail.com>
2021-08-23 21:30:23 +0300

    1 version.

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -11,11 +11,16 @@
 import os
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 
-im = Image.open("D:\\Users\\Kokoc\\PycharmProjects\\SkillBox_2018_Sliv\\lesson_013\\images\\ticket_template.png")
-print(im.size)
+
 def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+    picture = Image.open("D:\\Users\\Kokoc\\PycharmProjects\\SkillBox_2018_Sliv\\lesson_013\\images\\ticket_template.png")
+    imdraw = ImageDraw.Draw(picture)
+    font = ImageFont.truetype("arial.ttf", size=18)
+    imdraw.text((45, 120), fio, font=font, fill=ImageColor.colormap['black'])
+    imdraw.text((45, 190), from_, font=font, fill=ImageColor.colormap['black'])
+    imdraw.text((45, 258), to, font=font, fill=ImageColor.colormap['black'])
+    imdraw.text((275, 258), date, font=font, fill=ImageColor.colormap['black'])
+    picture.show()
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля agrparse.
@@ -26,3 +31,5 @@ def make_ticket(fio, from_, to, date):
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+make_ticket("Kalimullin Artur", "Saint-P", "SLV", "23/08/21")
