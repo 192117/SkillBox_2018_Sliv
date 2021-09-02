248912a042be350066d00543b337d8020761c36f
Kokoc <noti1996@gmail.com>
2021-09-02 20:04:11 +0300

    Finish bowling.

--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -1,11 +1,58 @@
 import random
 
+class OddError(Exception):
+    pass
+
+class ManyError(Exception):
+    pass
+
+
 class Bowl:
 
     def __init__(self):
-        game_result = list()
-        skittle = 10
+        self.game_result = ""
+        self.skittle = 10
+        self.result_sum = 0
 
     def get_result(self):
+        skittles = random.randint(0, 10)
+        if skittles == 0:
+            return "/"
+        elif skittles == 10:
+            return "X"
+        else:
+            return str(skittles)
+
+
+    def get_game_result(self):
         for frame in range(1, 11):
-            pass
+            self.game_result += self.get_result() + self.get_result()
+        return self.game_result
+
+    def get_score(self):
+        try:
+            if len(self.game_result) % 2 != 0:
+                raise OddError("Нечетное количество бросков")
+            score = list()
+            score.append(self.game_result[i:i+2] for i in range(0, len(self.game_result), 2))
+            for result in score:
+                summ = 0
+                for skittle1 in result:
+                    if skittle1 == "/":
+                        summ += 0
+                    elif skittle1 == "X":
+                        summ += 10
+                    else:
+                        summ += int(skittle1)
+                if summ > self.skittle:
+                    raise ManyError("Сбил больше, чем есть")
+                self.result_sum += summ
+            return self.result_sum
+        except Exception as ex:
+            return ex
+
+if __name__=="__main__":
+    bow = Bowl()
+    bow.get_game_result()
+    bow.get_score()
+
---
+++ lesson_014/tests/test_bowling.py
@@ -0,0 +1,13 @@
+import unittest
+
+class MyBowlingTest(unittest.TestCase):
+
+    def test_len(self):
+        pass
+
+    def test_count(self):
+        pass
+
+
+
+
